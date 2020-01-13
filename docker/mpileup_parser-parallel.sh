9f3f6b1030752c48e28af7b9bf99e8720129e6f2
Michele <berselli.michele@gmail.com>
2020-01-13 15:24:07 -0500

    .

--- docker/mpileup_parser-parallel.sh
+++ docker/mpileup_parser-parallel.sh
@@ -13,7 +13,7 @@ directory=mpileups/
 mkdir -p $directory
 
 # creating basic command
-command="python mpileup_parser.py -i $inputbam -r $reference --region {} -o ${directory}bam_{}.out"
+command="mpileup_parser.py -i $inputbam -r $reference --region {} -o ${directory}bam_{}.out"
 
 # running command
 cat $chromosomefile | parallel --halt 2 --jobs $nthreads $command || exit 1
