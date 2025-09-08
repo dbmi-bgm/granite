b292653722b951bcb305ca3b4b982a7f23ce55ae
Michele Berselli <berselli.michele@gmail.com>
2025-09-08 12:09:40 -0400

    .

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Harvard Medical School'
 author = 'Michele Berselli, Phil Grayson'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.1'
+release = '0.4.0'
 
 
 # -- General configuration ---------------------------------------------------
